bdf7997d19bc11c1db952d68711ec1e454a9946c
theprimone <yuns.xie@qq.com>
2021-03-08 23:12:22 +0800

    feat: import antd

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,40 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import vitePluginImp from 'vite-plugin-imp';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pinyin/',
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [
+    reactRefresh(),
+    tsconfigPaths(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => [`antd/lib/style/index.less`, `antd/lib/${name}/style/index.less`],
+        },
+      ],
+    }),
+  ],
   server: {
     open: true,
     port: 8000,
   },
-  alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
+    },
+    preprocessorOptions: {
+      less: {
+        modifyVars: { 'primary-color': '#13c2c2' },
+        javascriptEnabled: true,
+      },
     },
   },
 });
